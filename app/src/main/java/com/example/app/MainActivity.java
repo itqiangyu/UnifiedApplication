fac99bb3b12c3e05b2c3c34a619d517011c38b3c
itqiangyu <823631154@qq.com>
2020-02-20 23:09:39 +0800

    第一版本：实现登录功能

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        init();
+    }
+
+    private void init() {
+        tv_back = findViewById(R.id.tv_back);
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //登录界面销毁
+                MainActivity.this.finish();
+                Intent intent = new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
