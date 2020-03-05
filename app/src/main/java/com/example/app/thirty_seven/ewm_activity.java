917d125da6c11e39bc24743268a7f00687143aa3
G1reen <1509392470@qq.com>
2020-03-05 15:14:35 +0800

    第五版本：实现二维码支付功能(优化)

--- app/src/main/java/com/example/app/thirty_seven/ewm_activity.java
+++ app/src/main/java/com/example/app/thirty_seven/ewm_activity.java
@@ -42,12 +42,13 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 public class ewm_activity extends AppCompatActivity {
 
 
-    private LinearLayout x, x1, y, y1;
-    private RelativeLayout c;
+    private LinearLayout x, x1;
     private ConstraintLayout quanping;
+    private LinearLayout main;
 
     private TextView bianhao;
     private TextView jine;
+
     private Button button;
     private EditText editText;
 
@@ -67,25 +68,21 @@ public class ewm_activity extends AppCompatActivity {
 
         spinner = findViewById(R.id.spinner);
 
-
+        main = findViewById(R.id.mian);
         //spinner列表
         final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.carnum, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
         x = findViewById(R.id.linearLayout_zhifu);
-        y = findViewById(R.id.linearLayout_zhifu2);
         x1 = findViewById(R.id.lineneiron);
-        y1 = findViewById(R.id.lineneiron2);
-        c = findViewById(R.id.ewmtp);
         quanping = findViewById(R.id.quanpingewm);
-
         sleep = findViewById(R.id.shuimian);
-        jine = findViewById(R.id.jine);
-        bianhao = findViewById(R.id.bianhao);
+        jine = findViewById(R.id.jine1);
+        bianhao = findViewById(R.id.bianhao1);
         button = findViewById(R.id.button1);
         editText = findViewById(R.id.editText);
-        imageView = findViewById(R.id.ewm2);
+        imageView = findViewById(R.id.ewm22);
         ewm2 = findViewById(R.id.ewm3);
 
 
@@ -94,15 +91,18 @@ public class ewm_activity extends AppCompatActivity {
             public void onClick(View v) {
                 final String textContent = editText.getText().toString();
                 final String textContent1 = sleep.getText().toString();
-                final int w = Integer.parseInt(textContent1);
                 if (TextUtils.isEmpty(textContent)) {
                     Toast.makeText(ewm_activity.this, "您的输入为空!", Toast.LENGTH_SHORT).show();
                     return;
+                }
+                if (TextUtils.isEmpty(textContent1)) {
+                    Toast.makeText(ewm_activity.this, "二维码更新周期不能为空!", Toast.LENGTH_SHORT).show();
+                    return;
                 } else {
                     x1.setVisibility(View.GONE);
-                    y1.setVisibility(View.VISIBLE);
-                    c.setVisibility(View.VISIBLE);
+                    main.setVisibility(View.VISIBLE);
                     imageView.setVisibility(View.VISIBLE);
+                    final int w = Integer.parseInt(textContent1);
                     Timer timer = new Timer();
                     TimerTask task = new TimerTask() {
                         @Override
@@ -113,7 +113,7 @@ public class ewm_activity extends AppCompatActivity {
                                     mBitmap = CodeUtils.createImage(textContent, 400, 400, BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
                                     imageView.setImageBitmap(mBitmap);
                                     ewm2.setImageBitmap(mBitmap);
-                                    //System.out.println("刷新二维码了");
+                                    System.out.println("刷新二维码了");
                                 }
                             });
                         }
@@ -128,18 +128,16 @@ public class ewm_activity extends AppCompatActivity {
             public boolean onLongClick(View view) {
                 // Toast.makeText(ewm_activity.this, "wwww", Toast.LENGTH_SHORT).show();
                 bianhao.setText("车辆编号：" + spinner.getSelectedItem().toString());
-                jine.setText("付费金额：" + editText.getText().toString());
+                jine.setText("付费金额：" + editText.getText().toString() + "元");
                 return true;
             }
         });
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                x1.setVisibility(View.GONE);
+               x1.setVisibility(View.GONE);
                 x.setVisibility(View.GONE);
-                y.setVisibility(View.GONE);
-                y1.setVisibility(View.GONE);
-                c.setVisibility(View.GONE);
+                main.setVisibility(View.GONE);
                 if (quanping.getVisibility() == View.GONE) {
                     quanping.setVisibility(View.VISIBLE);
                 }
@@ -150,9 +148,7 @@ public class ewm_activity extends AppCompatActivity {
             public void onClick(View view) {
                 if (quanping.getVisibility() == View.VISIBLE) {
                     quanping.setVisibility(View.GONE);
-                    y.setVisibility(View.VISIBLE);
-                    y1.setVisibility(View.VISIBLE);
-                    c.setVisibility(View.VISIBLE);
+                    main.setVisibility(View.VISIBLE);
                 }
             }
         });
